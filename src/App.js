76a4cb24f2ad9018ef42c981086ff8037b6675ab
alanwu4321 <41980722+alanwu4321@users.noreply.github.com>
2019-05-21 14:44:58 -0400

    Add API Key to ENV variable

--- src/App.js
+++ src/App.js
@@ -12,10 +12,8 @@ import './App.css';
 import ColorDetect from './components/ColorDetect/ColorDetect';
 import CelebDetect from './components/CelebDetect/CelebDetect';
 
-
-//You must add your own API key here from Clarifai.
 const app = new Clarifai.App({
-  apiKey: '7bdeb36e5fb44b7ea0e70b8c732c4135'
+  apiKey: REACT_APP_API_KEY
 });
 
 const particlesOptions = {
@@ -51,18 +49,9 @@ class App extends Component {
     }
   }
 
+  //Configure Env varibles
   componentDidMount() {
     require('dotenv').config()
-    console.log(process.env)
-    //  this.onFaceDetect()
-    // fetch('https://ktntxy148c.execute-api.us-east-1.amazonaws.com/dev', {
-    //           method: 'get',
-    //         })
-    //           .then(response => response.json())
-    //           .then(count => {
-    //             console.log(count)
-    //           })
-
   }
 
   loadUser = (data) => {
@@ -90,8 +79,81 @@ class App extends Component {
       rightCol: width - (clarifaiFace.right_col * width),
       bottomRow: height - (clarifaiFace.bottom_row * height)
     }
+  }
 
+  //Duplicate input image on Canvas and store the "face" in AWS S3 Bucket
+  duplicateImage = (box, callback) => {
 
+    //Dom elements
+    const tempCanvas = document.getElementById("canvasTemp");
+    const faceCanvas = document.getElementById("canvasFace");
+    const image = document.getElementById('inputimage');
+
+    //Convert to Canvas
+    var ctx = tempCanvas.getContext("2d");
+    var ctxFace = faceCanvas.getContext("2d");
+    var BoxWidth = 500 - box.leftCol - box.rightCol
+    var BoxHeight = image.height - box.bottomRow - box.topRow
+
+    ctx.drawImage(image, 0, 0, image.width, image.height);
+
+    //Print face ImageData on new Canvas 
+    var imageData = ctx.getImageData(box.leftCol, box.topRow, BoxWidth, BoxHeight);
+    ctxFace.putImageData(imageData, 0, 0);
+
+    //Pass the DataURL to server for S3 storing
+    var faceCanvasData = faceCanvas.toDataURL();
+
+    //onFaceDetect will return a S3 URL Key
+    //Pass the Key in API to detect color and celeb match
+    this.onFaceDetect(faceCanvasData, (res) => {
+      var url = process.env.REACT_APP_S3_ENDPOINT + res
+      app.models
+        .predict(
+          'e466caa0619f444ab97497640cefc4dc',
+          url)
+        .then(response => {
+          var celebMatch = response.outputs[0].data.regions[0].data.concepts[0].name
+          this.setState({ celebMatch: { name: celebMatch } })
+        })
+
+      app.models
+        .predict(
+          Clarifai.COLOR_MODEL,
+          url)
+        .then(response => {
+
+          var convert = require('color-convert');
+          //get the hex and rgb output for the API
+          const raw_hex = response.outputs[0].data.colors[0].raw_hex
+          const api_rgb = convert.hex.rgb(raw_hex)
+          const hex_name = response.outputs[0].data.colors[0].w3c.name
+
+          // only visit every 5 pixels
+          var blockSize = 5, 
+            i = -4,
+            length,
+            rgb = { r: 0, g: 0, b: 0 },
+            count = 0;
+          length = imageData.data.length;
+          while ((i += blockSize * 4) < length) {
+            ++count;
+            rgb.r += imageData.data[i];
+            rgb.g += imageData.data[i + 1];
+            rgb.b += imageData.data[i + 2];
+          }
+          rgb.r = ~~(rgb.r / count);
+          rgb.g = ~~(rgb.g / count);
+          rgb.b = ~~(rgb.b / count);
+
+          //Average results I calculated and the results return by API
+          var averageRed = (rgb.r + api_rgb[0]) / 2
+          var averageGreen = (rgb.g + api_rgb[1]) / 2
+          var averageBlue = (rgb.b + api_rgb[2]) / 2
+          var average_dominateHex = "#" + convert.rgb.hex(averageRed, averageGreen, averageBlue)
+          this.setState({ dominateColor: { hex: average_dominateHex, name: hex_name, red: averageRed / 254 } });
+        })
+    })
   }
 
   displayFaceBox = (box) => {
@@ -146,7 +208,6 @@ class App extends Component {
       .then(response => response.json())
       .then(inputURL => {
         var url = process.env.REACT_APP_S3_ENDPOINT + inputURL.fileName
-
         callback(url)
       })
   }
@@ -167,66 +228,8 @@ class App extends Component {
   }
 
 
-  duplicateImage = (box, callback) => {
-    const tempCanvas = document.getElementById("canvasTemp");
-    // const canvas = document.getElementById("canvasID");
-    const faceCanvas = document.getElementById("canvasFace");
-    var ctx = tempCanvas.getContext("2d");
-    var ctxFace = faceCanvas.getContext("2d");
-    var BoxWidth = 500 - box.leftCol - box.rightCol
-    const image = document.getElementById('inputimage');
-    var BoxHeight = image.height - box.bottomRow - box.topRow
-    ctx.drawImage(image, 0, 0, image.width, image.height);
-    var imageData = ctx.getImageData(box.leftCol, box.topRow, BoxWidth, BoxHeight);
-    ctxFace.putImageData(imageData, 0, 0);
-    var faceCanvasData = faceCanvas.toDataURL();
-    this.onFaceDetect(faceCanvasData, (res) => {
-      var url = process.env.REACT_APP_S3_ENDPOINT + res
-      app.models
-        .predict(
-          'e466caa0619f444ab97497640cefc4dc',
-          url)
-        .then(response => {
-          var celebMatch = response.outputs[0].data.regions[0].data.concepts[0].name
-          this.setState({ celebMatch: { name: celebMatch } })
-        })
-
-      app.models
-        .predict(
-          Clarifai.COLOR_MODEL,
-          url)
-        .then(response => {
-          var convert = require('color-convert');
-          const raw_hex = response.outputs[0].data.colors[0].raw_hex
-          const api_rgb = convert.hex.rgb(raw_hex)
-          debugger
-          const hex_name = response.outputs[0].data.colors[0].w3c.name
-          var blockSize = 5, // only visit every 5 pixels
-            i = -4,
-            length,
-            rgb = { r: 0, g: 0, b: 0 },
-            count = 0;
-          length = imageData.data.length;
-          while ((i += blockSize * 4) < length) {
-            ++count;
-            rgb.r += imageData.data[i];
-            rgb.g += imageData.data[i + 1];
-            rgb.b += imageData.data[i + 2];
-          }
-          rgb.r = ~~(rgb.r / count);
-          rgb.g = ~~(rgb.g / count);
-          rgb.b = ~~(rgb.b / count);
-          var averageRed = (rgb.r + api_rgb[0]) / 2
-          var averageGreen = (rgb.g + api_rgb[1]) / 2
-          var averageBlue = (rgb.b + api_rgb[2]) / 2
-          var average_dominateHex = "#" + convert.rgb.hex(averageRed, averageGreen, averageBlue)
-          debugger
-
-          this.setState({ dominateColor: { hex: average_dominateHex, name: hex_name, red:averageRed/254} });
-        })
-    })
-  }
   
+
 
   onRouteChange = (route) => {
     if (route === 'signout') {
